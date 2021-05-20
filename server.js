e040eb159202c1118ba6df5ed49ca27d48c6b800
evgen <e.loshkarev@englishdom.com>
2021-05-20 12:35:22 +0300

    test

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+app.set('view engine', 'ejs');
+const io = require('socket.io')(server);
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+app.use('/peerjs', peerServer);
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('room', { roomId: 'sessionId' });
+});
+
+io.on('connection', (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join('sessionId');
+    socket.to('sessionId').broadcast.emit("user-connected", userId);
+  });
+});
+
+server.listen(3000);
