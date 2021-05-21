88b01d88e2114bd71d0063a9b4bf8eddfe91bb7b
evgen <e.loshkarev@englishdom.com>
2021-05-21 16:58:18 +0300

    Updates

--- server.js
+++ server.js
@@ -19,23 +19,8 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   socket.on("join-room", (roomId, userId) => {
     socket.join('sessionId');
-    socket.to('sessionId').broadcast.emit("user-connected", userId);
+    socket.to('sessionId').broadcast.emit("user-connected", userId, roomId);
   });
 });
 
 server.listen(3000);
-
-// config: {"iceServers": [
-      
-//   {
-//     "iceTransportPolicy": "relay",
-//     "urls": "stun:62.138.7.233:3478"
-//   },
-//   {
-//     "iceTransportPolicy": "relay",
-//     "urls": "turn:62.138.7.233:3478",
-//     "username": "ninefingers",
-//     "credential": "youhavetoberealistic"
-//   }
-// ],
-// }
